4c9a51c706bd77cf0c3af9e38a81e5fefd8664f5
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-08-06 11:59:15 -0700

    Add prominentcolor

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wallpaper Constructor
+
+A command line tool written in go that takes in images and outputs resized images 
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"strings"
 
+	"github.com/EdlinOrg/prominentcolor"
 	"github.com/disintegration/imaging"
 )
 
@@ -15,6 +16,10 @@ func main() {
 	fileName := flag.String("file", "img.jpeg", "image to turn into wallpaper")
 	fileNameOut := flag.String("out", "wallpaper-output.png", "file for output")
 	blurRadius := flag.Int("blur", 10, "blur radius for background; suggested: 10 - 50 \n\t(the higher the blur radius, the longer it takes to run)")
+	w := flag.Int("w", 1440, "The width of the output image")
+	h := flag.Int("h", 900, "The height of the output image")
+	width := flag.Int("width", 1440, "The width of the output image")
+	height := flag.Int("height", 900, "The height of the output image")
 	flag.Parse()
 	tail := flag.Args()
 	if len(tail) > 0 && *fileName == "img.jpeg" {
@@ -24,6 +29,12 @@ func main() {
 		*fileNameOut = tail[1]
 	} else if *fileNameOut == "wallpaper-output.png" {
 		*fileNameOut = strings.Split(*fileName, ".")[0] + "-wallpaper.png"
+	}
+	if *w != 1440 {
+		width = w
+	}
+	if *h != 900 {
+		height = h
 	}
 	fmt.Printf("\tInput File: %s\n\tOutput File: %s\n\tBlur Radius: %v\n", *fileName, *fileNameOut, *blurRadius)
 	// Decode the JPEG data. If reading from file, create a reader with
@@ -35,25 +46,35 @@ func main() {
 
 	blur := make(chan image.Image)
 	foreground := make(chan image.Image)
+	background := make(chan image.Image)
 
 	go func(image image.Image) { // blur
 		blurred := imaging.Blur(image, float64(*blurRadius))
-		blurred = imaging.Fill(blurred, 1440, 900, imaging.Center, imaging.Linear)
+		blurred = imaging.Fill(blurred, *width, *height, imaging.Center, imaging.Linear)
 		fmt.Println("Image Blurred")
 		blur <- blurred
 	}(img)
 
 	go func(image image.Image) { // foreground
-		resized := imaging.Fit(image, 1440, 900, imaging.Lanczos)
-		resized = resizeToIsh(resized, 1440, 900, imaging.Lanczos)
+		resized := imaging.Fit(image, *width, *height, imaging.Lanczos)
+		resized = resizeToIsh(resized, *width, *height, imaging.Lanczos)
 		fmt.Println("Image resized")
 		foreground <- resized
 	}(img)
 
+	go func(image image.Image) { // foreground
+		promColors, err := prominentcolor.Kmeans(img)
+		promColor := promColors[0].Color
+		if err != nil {
+			log.Fatalf("prominentcolor failed: %v", err)
+		}
+		fmt.Printf("Prominent colors: %v\n", promColors)
+		background <- imaging.New(*width, *height, color.NRGBA{uint8(promColor.R), uint8(promColor.G), uint8(promColor.B), 200})
+	}(img)
+
 	// put it all together
-	blurred := <-blur
-	out := imaging.New(1440, 900, color.NRGBA{0, 0, 0, 80})
-	out = imaging.OverlayCenter(out, blurred, 0.5)
+	out := <-background
+	out = imaging.OverlayCenter(out, <-blur, 0.5)
 	out = imaging.PasteCenter(out, <-foreground)
 	fmt.Println("Composite compiled")
 
