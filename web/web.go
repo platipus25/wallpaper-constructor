bcc15b0147dfee1e41bcd5bdc145243287437076
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-08-29 13:05:25 -0700

    Web version works? Ish

--- web/web.go
+++ web/web.go
@@ -1,15 +1,15 @@
-package wallpaperconstructor
+package main
 
 import (
 	"bytes"
 	"encoding/base64"
 	"fmt"
 	"image"
+	"syscall/js"
 
 	// import for side effects to register jpeg image format
 	_ "image/jpeg"
 	"image/png"
-	"io/ioutil"
 	"log"
 	"strings"
 
@@ -17,16 +17,31 @@ import (
 )
 
 func main() {
-	fmt.Println("start")
+	/*fmt.Println("start")
 	data := ""
 	out := Process(1440, 900, data, 20)
 
 	err := ioutil.WriteFile("out.txt", []byte(out), 0644)
 	if err != nil {
 		log.Fatalf("Oops:", err)
-	}
+	}*/
+	holdOpen := make(chan struct{}, 0)
 
+	fmt.Println("Hello Web,\n\tGolang")
+	var fun js.Func
+	fun = js.FuncOf(jsProcess)
+
+	js.Global().Set("processImg", fun)
+
+	<-holdOpen
 	//fmt.Println(out)
+}
+
+func jsProcess(this js.Value, i []js.Value) interface{} {
+	fmt.Println("Processing...")
+	outStr := Process(i[0].Int(), i[1].Int(), i[2].String(), i[3].Int())
+	fmt.Println("Done.")
+	return js.ValueOf(outStr)
 }
 
 // Process takes in a base64 uses wallpaperconstructor.ProcessImg(width, height, img, blurRadius
