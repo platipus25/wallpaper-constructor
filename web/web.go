2f3b758231cb2d25d0de6979ee888d269352535f
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-09-27 14:12:52 -0700

    Restructure web

--- web/web.go
+++ web/web.go
@@ -1,11 +1,10 @@
-package main
+package web
 
 import (
 	"bytes"
 	"encoding/base64"
 	"fmt"
 	"image"
-	"syscall/js"
 
 	// import for side effects to register jpeg image format
 	_ "image/jpeg"
@@ -15,34 +14,6 @@ import (
 
 	wallpaperconstructor "github.com/platipus25/wallpaperconstructor/process"
 )
-
-func main() {
-	/*fmt.Println("start")
-	data := ""
-	out := Process(1440, 900, data, 20)
-
-	err := ioutil.WriteFile("out.txt", []byte(out), 0644)
-	if err != nil {
-		log.Fatalf("Oops:", err)
-	}*/
-	holdOpen := make(chan struct{}, 0)
-
-	fmt.Println("Hello Web,\n\tGolang")
-	var fun js.Func
-	fun = js.FuncOf(jsProcess)
-
-	js.Global().Set("processImg", fun)
-
-	<-holdOpen
-	//fmt.Println(out)
-}
-
-func jsProcess(this js.Value, i []js.Value) interface{} {
-	fmt.Println("Processing...")
-	outStr := Process(i[0].Int(), i[1].Int(), i[2].String(), i[3].Int())
-	fmt.Println("Done.")
-	return js.ValueOf(outStr)
-}
 
 // Process takes in a base64 uses wallpaperconstructor.ProcessImg(width, height, img, blurRadius
 func Process(width, height int, imgStr string, blurRadius int) string {
